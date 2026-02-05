2720747ecf0f1c7907109d68abf713a2248fd3d5
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2026-02-05 02:56:35 +0000

    :art: Refactor Home component to handle image uploads and display success messages

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,52 +1,81 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
-import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
 
-	const { store, dispatch } = useGlobalReducer()
 
-	const loadMessage = async () => {
+	const cloudName = "dd0wschpy";
+	const uploadPreset = "chococapibara";
+
+	const cloudinaryUlr = () => `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
+
+	const [file, setFile] = useState(null);
+
+	const [successMessage, setSuccessMessage] = useState("");
+	const [publicImageUrl, setPublicImageUrl] = useState("");
+	// base64 es un string que mantiene la informacion de archivos multimedia largos
+	// se puede usar para mostrar imagenes sin necesidad de subirlas a un servidor
+
+	const handleUploadImage = async (event) => {
+		event.preventDefault();
+
+		const formData = new FormData();
+
+		formData.append("file", file);
+		formData.append("upload_preset", uploadPreset);
+
 		try {
-			const backendUrl = import.meta.env.VITE_BACKEND_URL
-
-			if (!backendUrl) throw new Error("VITE_BACKEND_URL is not defined in .env file")
-
-			const response = await fetch(backendUrl + "/api/hello")
-			const data = await response.json()
-
-			if (response.ok) dispatch({ type: "set_hello", payload: data.message })
-
-			return data
-
+			const resp = await fetch(cloudinaryUlr(), {
+				method: "POST",
+				// headers: {
+				// 	"Content-Type": "multipart/form-data"
+				// },
+				body: formData
+			});
+			const data = await resp.json();
+			setPublicImageUrl(data.url);
+			setSuccessMessage("Image uploaded successfully!");
 		} catch (error) {
-			if (error.message) throw new Error(
-				`Could not fetch the message from the backend.
-				Please check if the backend is running and the backend port is public.`
-			);
+			console.error("Error uploading image to Cloudinary:", error);
 		}
 
-	}
+
+	};
 
 	useEffect(() => {
-		loadMessage()
 	}, [])
 
 	return (
 		<div className="text-center mt-5">
-			<h1 className="display-4">Hello Rigo!!</h1>
 			<p className="lead">
-				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
+				{file && !publicImageUrl && <img src={file ? URL.createObjectURL(file) : rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby"
+					width={200} height={200}
+					style={{ aspectRatio: "1 / 1", objectFit: "cover" }}
+				/>}
+
+				{
+					successMessage && <div className="alert alert-success" role="alert">
+						{successMessage}
+					</div>
+				}
+
+				{
+					publicImageUrl && <div>
+						<p>Uploaded Image:</p>
+						<img src={publicImageUrl + `?w=200&h=200&c=fill`} className="img-fluid mb-3" alt="Uploaded" width={200} height={200}
+							style={{ aspectRatio: "1 / 1", objectFit: "cover" }}
+						/>
+						<p><a href={publicImageUrl} target="_blank" rel="noopener noreferrer">{publicImageUrl}</a></p>
+					</div>
+				}
 			</p>
-			<div className="alert alert-info">
-				{store.message ? (
-					<span>{store.message}</span>
-				) : (
-					<span className="text-danger">
-						Loading message from the backend (make sure your python 🐍 backend is running)...
-					</span>
-				)}
-			</div>
+			<form onSubmit={handleUploadImage}>
+				<div className="mb-3">
+					<label htmlFor="formFile" className="form-label">Default file input example</label>
+					<input className="form-control" type="file" onChange={event => setFile(event.target.files[0])} />
+				</div>
+				<button className="btn btn-primary" type="submit">Upload</button>
+			</form>
 		</div>
 	);
 }; 
